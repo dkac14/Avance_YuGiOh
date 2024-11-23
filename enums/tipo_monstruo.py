9ace12d45f6b64959c91630e7e23e71aa75b5e76
ZuzCrip <xxguardiangamerhd@outlook.com>
2024-11-23 16:10:04 -0500

    cambio en las variables de tipo_monstruos

---
+++ enums/tipo_monstruo.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class TipoMonstruo(Enum):
+    L = "Lanzador de Conjuros "
+    D = "Dragón"
+    Z = "Zombi "
+    G = "Guerrero"
+    B = "Bestia"
+    O = "Demonio"
+    H = "Hidropotamo"

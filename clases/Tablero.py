85dc273e93ac8d3511b26765ca5fcea04df894b0
Dommmmmm <domenicaamores@gmail.com>
2024-11-24 16:50:54 -0500

    actualizando tablero

--- clases/Tablero.py
+++ clases/Tablero.py
@@ -5,54 +5,48 @@ import numpy as np
 
 class Tablero:
     def __init__(self):
-        self.tablero = np.full((2, 3), None)
-        self.cartas_monstruo = 0
-        self.cartas_Magica_Trampa = 0
+        self.turno = 1
+        self.tablero_monstruos = [None] * 3  # Tres espacios para monstruos
+        self.tablero_magicas_trampas = [None] * 3  # Tres espacios para mágicas/trampas
 
     def agregar_carta_monstruo(self, carta):
-        if self.cartas_monstruo < 3 and isinstance(carta, CartaMonstruo):
-            for fila in range(2):  
-                for columna in range(3):  
-                    if self.tablero[fila][columna] is None:  
-                        self.tablero[fila][columna] = carta  
-                        self.cartas_monstruo += 1
-                        return 
+        if isinstance(carta, CartaMonstruo):
+            for i in range(3):
+                if self.tablero_monstruos[i] is None:
+                    self.tablero_monstruos[i] = carta
+                    return True
             print("No hay espacio para más cartas de monstruo en el tablero.")
+        return False
 
     def agregar_carta_trampa(self, carta):
-        if self.cartas_Magica_Trampa < 3 and isinstance(carta, CartaTrampa):
-            for fila in range(2):  
-                for columna in range(3):  
-                    if self.tablero[fila][columna] is None:  
-                        self.tablero[fila][columna] = carta  
-                        self.cartas_Magica_Trampa += 1
-                        return 
-            print("No hay espacio para más cartas de trampa/mágica en el tablero.")
+        if isinstance(carta, CartaTrampa):
+            for i in range(3):
+                if self.tablero_magicas_trampas[i] is None:
+                    self.tablero_magicas_trampas[i] = carta
+                    return True
+            print("No hay espacio para más cartas trampa en el tablero.")
+        return False
 
     def agregar_carta_magica(self, carta):
-        if self.cartas_Magica_Trampa < 3 and isinstance(carta, CartaMagica):
-            for fila in range(2):  
-                for columna in range(3):  
-                    if self.tablero[fila][columna] is None:  
-                        self.tablero[fila][columna] = carta  
-                        self.cartas_Magica_Trampa += 1
-                        return 
-            print("No hay espacio para más cartas de trampa/mágica en el tablero.")
+        if isinstance(carta, CartaMagica):
+            for i in range(3):
+                if self.tablero_magicas_trampas[i] is None:
+                    self.tablero_magicas_trampas[i] = carta
+                    return True
+            print("No hay espacio para más cartas mágicas en el tablero.")
+        return False
 
+    def obtener_cartas_monstruo(self):
+        return [carta for carta in self.tablero_monstruos if carta]
 
-    def obtenerCartasTrampa(self):
-        trampas = []
-        for fila in range(2):  
-            for columna in range(3):  
-                if isinstance(self.tablero[fila][columna], CartaTrampa):
-                    trampas.append(self.tablero[fila][columna])
-        return trampas
-    
+    def obtener_cartas_magicas(self):
+        return [carta for carta in self.tablero_magicas_trampas if isinstance(carta, CartaMagica)]
 
-    def obtenerCartasMágicas(self):
-        magicas = []
-        for fila in range(2):  
-            for columna in range(3):  
-                if isinstance(self.tablero[fila][columna], CartaMagica):
-                    magicas.append(self.tablero[fila][columna])
-        return magicas 
+    def obtener_cartas_trampa(self):
+        return [carta for carta in self.tablero_magicas_trampas if isinstance(carta, CartaTrampa)]
+
+    def eliminar_carta(self, carta):
+        if carta in self.tablero_monstruos:
+            self.tablero_monstruos[self.tablero_monstruos.index(carta)] = None
+        elif carta in self.tablero_magicas_trampas:
+            self.tablero_magicas_trampas[self.tablero_magicas_trampas.index(carta)] = None

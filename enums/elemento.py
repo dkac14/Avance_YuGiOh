62507a6202e6465828c16d6711af5b6e9bf6b315
dkac14 <150403101+dkac14@users.noreply.github.com>
2024-11-27 02:10:11 -0500

    Delete enums directory

--- enums/elemento.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Elemento(Enum):
-    OSCURIDAD = "Oscuridad"
-    LUZ = "Luz"
-    TIERRA = "Tierra"
-    AGUA = "Agua"
-    FUEGO = "Fuego"
-    VIENTO = "Viento"
--- enums/tipo_monstruo.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class TipoMonstruo(Enum):
-    L = "Lanzador de Conjuros "
-    D = "Dragón"
-    Z = "Zombi "
-    G = "Guerrero"
-    B = "Bestia"
-    O = "Demonio"
